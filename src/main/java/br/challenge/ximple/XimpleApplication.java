7a09395d933132093a4c285f2e7e63d3bf8632c6
jhoestevam <jhoestevam1@gmail.com>
2024-04-22 15:05:39 -0300

    init commit

---
+++ src/main/java/br/challenge/ximple/XimpleApplication.java
@@ -0,0 +1,13 @@
+package br.challenge.ximple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/challenge/ximple/XimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.challenge.ximple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
